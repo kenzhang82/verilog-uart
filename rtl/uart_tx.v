881719bc6a1659a021588d7111b99fd5d14251ba
Ken Zhang <ken.zhang028@gmail.com>
2020-04-25 19:33:08 +1000

    * Rewrite Tx in FSM

--- rtl/uart_tx.v
+++ rtl/uart_tx.v
@@ -60,56 +60,92 @@ module uart_tx #
     input  wire [15:0]            prescale
 );
 
-reg s_axis_tready_reg = 0;
+    localparam [2:0] IDLE      = 2'b00,
+                     START_BIT = 2'b01,
+                     DATA_BITS = 2'b10,
+                     STOP_BIT  = 2'b11;
 
-reg txd_reg = 1;
+    reg [1:0] state = IDLE;
+    reg s_axis_tready_reg = 0;
+    reg [DATA_WIDTH-1:0] s_axis_tdata_reg = 0;
+    reg txd_reg = 1;
+    reg [15:0] prescale_reg = 0;
+    reg [3:0] bit_cnt = 0;
 
-reg busy_reg = 0;
-
-reg [DATA_WIDTH:0] data_reg = 0;
-reg [18:0] prescale_reg = 0;
-reg [3:0] bit_cnt = 0;
-
-assign s_axis_tready = s_axis_tready_reg;
-assign txd = txd_reg;
-
-assign busy = busy_reg;
-
-always @(posedge clk) begin
-    if (rst) begin
-        s_axis_tready_reg <= 0;
-        txd_reg <= 1;
-        prescale_reg <= 0;
-        bit_cnt <= 0;
-        busy_reg <= 0;
-    end else begin
-        if (prescale_reg > 0) begin
+    always @(posedge clk) begin
+        if (rst) begin
+            state <= IDLE;
             s_axis_tready_reg <= 0;
-            prescale_reg <= prescale_reg - 1;
-        end else if (bit_cnt == 0) begin
-            s_axis_tready_reg <= 1;
-            busy_reg <= 0;
-
-            if (s_axis_tvalid) begin
-                s_axis_tready_reg <= !s_axis_tready_reg;
-                prescale_reg <= (prescale << 3)-1;
-                bit_cnt <= DATA_WIDTH+1;
-                data_reg <= {1'b1, s_axis_tdata};
-                txd_reg <= 0;
-                busy_reg <= 1;
-            end
-        end else begin
-            if (bit_cnt > 1) begin
-                bit_cnt <= bit_cnt - 1;
-                prescale_reg <= (prescale << 3)-1;
-                {data_reg, txd_reg} <= {1'b0, data_reg};
-            end else if (bit_cnt == 1) begin
-                bit_cnt <= bit_cnt - 1;
-                prescale_reg <= (prescale << 3);
-                txd_reg <= 1;
-            end
+            s_axis_tdata_reg <= 0;
+            txd_reg <= 1;
+            prescale_reg <= 0;
+            bit_cnt <= 0;
+        end
+        else begin
+            case (state)
+                IDLE: begin
+                    if(prescale_reg < 1) begin
+                        prescale_reg <= prescale_reg + 1;
+                        s_axis_tready_reg <= 1;
+                    end
+                    else begin
+                        prescale_reg <= 0;
+                        s_axis_tready_reg <= 0;
+                        if (s_axis_tvalid) begin
+                            s_axis_tready_reg <= 0;
+                            s_axis_tdata_reg <= s_axis_tdata;
+                            state <= START_BIT;
+                        end
+                        else begin
+                            state <= IDLE;
+                        end
+                    end
+                end
+                START_BIT: begin
+                    // account for one more cycle for state transition
+                    if (prescale_reg < (prescale << 3) - 1) begin
+                        prescale_reg <= prescale_reg + 1;
+                        txd_reg <= 0;
+                    end
+                    else begin
+                        prescale_reg <= 0;
+                        state <= DATA_BITS;
+                    end
+                end
+                DATA_BITS: begin
+                    if (bit_cnt < DATA_WIDTH) begin
+                        if (prescale_reg < (prescale << 3) - 1) begin
+                            prescale_reg <= prescale_reg + 1;
+                            txd_reg <= s_axis_tdata_reg[bit_cnt];
+                        end
+                        else begin
+                            bit_cnt <= bit_cnt + 1;
+                            prescale_reg <= 0;
+                        end
+                    end
+                    else begin
+                        bit_cnt <= 0;
+                        state <= STOP_BIT;
+                    end
+                end
+                STOP_BIT: begin
+                    // account for one more cycle for state transition
+                    if (prescale_reg < (prescale << 3) - 1) begin
+                        prescale_reg <= prescale_reg + 1;
+                        txd_reg <= 1;
+                    end
+                    else begin
+                        prescale_reg <= 0;
+                        state <= IDLE;
+                    end
+                end
+                default: state <= IDLE;
+            endcase
         end
     end
-end
+
+    assign s_axis_tready = s_axis_tready_reg;
+    assign txd = txd_reg;
+    assign busy = (state != IDLE);
 
 endmodule
